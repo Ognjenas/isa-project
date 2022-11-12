3deef8e5e6e27d179d0ba609806a65999b5a9195
srdjan1210 <businessrdjan@gmail.com>
2022-11-12 17:10:45 +0100

    Refactored services with axios

--- public-app/blood-center/src/modules/surveys/services/survey.service.ts
+++ public-app/blood-center/src/modules/surveys/services/survey.service.ts
@@ -1,52 +1,44 @@
 import { toast } from "react-toastify"
+import axios from "axios"
 import { MakeSurveyDTO } from "../dtos/MakeSurveyDTO"
 
 export class SurveyService {
     private apiUrl: string = "http://localhost:8000"
+    
     constructor() {}
 
     async getQuestions() {
         const url = `${this.apiUrl}/surveys`
 
-        let response = await fetch(url, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        })
+        try {
+            let response = await axios.get(url)
+            return response.data.questions
+        } catch(e: any) {
+            toast.error("Something wrong with capturing questions!")
+            return []
+        }
 
-        if (!response.ok) throw new Error("Surveys doesn't exist!")
-        let data = await response.json()
-        return data.questions
     }
 
-    async makeSurvey(MakeSurveyDTO: MakeSurveyDTO) {
+    async makeSurvey(makeSurveyDTO: MakeSurveyDTO) {
         const url = `${this.apiUrl}/surveys`
 
         try {
-            let response = await fetch(url, {
-                method: "POST",
-                body: JSON.stringify(MakeSurveyDTO),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-
-            if (!response.ok) {
-                let error = await response.json()
-                this.parseError(error)
-            }
+            let response = await axios.post(url, makeSurveyDTO)
             toast.success("Successfully made survey!", { autoClose: 3000 })
             return true
         } catch (e: any) {
-            toast.error(e.message, { autoClose: 3000 })
+            const message = this.parseError(e.response.data)
+            toast.error(message, { autoClose: 3000 })
             return false
         }
     }
 
     parseError(error: any) {
         if (error.statusCode) {
-            throw new Error(error.message)
+            return error.message
         } else {
-            throw new Error("Date not correct!")
+            return 'Data not correct!'
         }
     }
 }
